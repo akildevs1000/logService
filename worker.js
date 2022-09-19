889a458cfb44fc2d9bdbd8d8a3d0211875106d8e
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2022-09-19 17:42:19 +0400

    fixes

--- worker.js
+++ worker.js
@@ -1,13 +1,13 @@
 const WebSocket = require("ws");
 require('dotenv').config()
 // const axios = require("axios").default;
-
+socket.open = () => console.log("connected\n");
 
 const fs = require("fs");
 let socket = new WebSocket(process.env.ENDPOINT);
 
 socket.onmessage = function ({ data }) {
-  //   console.log(JSON.parse(data));
+    console.log(JSON.parse(data));
   let {
     UserCode: UserID,
     DeviceID,
@@ -18,7 +18,7 @@ socket.onmessage = function ({ data }) {
   let str = `${UserID},${DeviceID},${LogTime},${SerialNumber}`;
   console.log(str);
   let path = "logs.csv";
-  fs.appendFileSync(path, str + "\n");
+  // fs.appendFileSync(path, str + "\n");
 
   // let Hash = '$2y$10$RQ0d7Yo1ad/aTm2pEx3QvuGatA6t0qqH76m7VXYGkNjzVYqNGAQ.K';
 
