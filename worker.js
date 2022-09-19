bee513c7f16d3e573977ffb00c0d6481fd307a05
akildevs1000 <107389124+akildevs1000@users.noreply.github.com>
2022-09-19 17:07:35 +0400

    first commit

---
+++ worker.js
@@ -0,0 +1,33 @@
+const WebSocket = require("ws");
+require('dotenv').config()
+// const axios = require("axios").default;
+
+
+const fs = require("fs");
+let socket = new WebSocket(process.env.ENDPOINT);
+
+socket.onmessage = function ({ data }) {
+  //   console.log(JSON.parse(data));
+  let {
+    UserCode: UserID,
+    DeviceID,
+    RecordDate: LogTime,
+    RecordNumber: SerialNumber,
+  } = JSON.parse(data).Data;
+
+  let str = `${UserID},${DeviceID},${LogTime},${SerialNumber}`;
+  console.log(str);
+  let path = "logs.csv";
+  fs.appendFileSync(path, str + "\n");
+
+  // let Hash = '$2y$10$RQ0d7Yo1ad/aTm2pEx3QvuGatA6t0qqH76m7VXYGkNjzVYqNGAQ.K';
+
+  // axios({
+  //   method: "post",
+  //   url: "http://192.168.2.174:8000/api/socket",
+  //   data: { UserID, DeviceID, LogTime, SerialNumber, Hash },
+  // }).then(function (response) {
+  //   console.log("server response");
+  //   console.log(response.data);
+  // });
+};
